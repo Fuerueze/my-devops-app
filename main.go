e2fb7511274d9a2ecda196f463fdca3c5e53be37
NhiNguyen <116640206+ngnhinguyen@users.noreply.github.com>
2024-09-08 19:16:38 +0200

    Update main.go

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
         name := os.Getenv("NAME")
         if name == "" {
-                name = "APFELLIEBE"
+                name = "APFELLIEBE!"
         }
         fmt.Fprintf(w, "Hello %s!\n", name)
 }
